845b898763a8d3333b6b71eac5d9459eee0b95b6
Alexey Gamov <lexa2009xxxx@yandex.ru>
2022-08-17 23:30:18 +0300

    feat: сделать фиксированное меню с бургером в мобайле

---
+++ src/components/header/header.js
@@ -0,0 +1,21 @@
+const header = document.querySelector(".header");
+const logo = header.querySelector(".header__logo");
+const burgerButton = header.querySelector('.header__burger-btn');
+const burgerMenu = header.querySelector('.burger-menu');
+const burgerCloseBtn = header.querySelector('.burger-menu__close-button');
+
+burgerButton.addEventListener('click', function() {
+	burgerMenu.classList.add('burger-menu_active');
+});
+
+burgerCloseBtn.addEventListener('click', function() {
+	burgerMenu.classList.remove('burger-menu_active');
+})
+
+document.addEventListener("scroll", function () {
+  if (window.pageYOffset >= 1) {
+    header.classList.add("header_fixed");
+  } else {
+    header.classList.remove("header_fixed");
+  }
+});
