9a076658943220709adb736f02671f24e1fe546e
maximle <mletunovskiy@yandex.ru>
2022-08-21 13:24:28 +0300

    feature: popup+js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,28 @@
 import './index.css';
+<<<<<<< HEAD
 import '../components/header/header.js';
 import '../components/horiz-scroll.js'
+=======
+import '../components/header/header.js'
+import { Popup } from '../components/Popup/Popup.js';
+import { popupSelectorCourse, popupSelectorPartner } from '../utils/constants';
+
+const popupCourseButton = document.querySelector('.partners__title');
+const popupCourse = new Popup({popupSelector: popupSelectorCourse});
+
+const popupPartnerButton = document.querySelector('.partners__border');
+const popupPartner = new Popup({popupSelector: popupSelectorPartner});
+
+console.log('asdasf');
+
+
+popupCourseButton.addEventListener('click', (e) =>{
+  e.preventDefault();
+  popupCourse.openPopup();
+})
+
+popupPartnerButton.addEventListener('click', (e) =>{
+  e.preventDefault();
+  popupPartner.openPopup();
+})
+>>>>>>> ea9485f (feature: popup+js)
