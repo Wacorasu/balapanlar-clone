252d9060c54e5c274a81c5a15555c079d36e5ec6
Platon Brazhnikov <90373473+BreadLinx@users.noreply.github.com>
2022-08-22 19:11:06 +0300

    links have been added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,16 +1,21 @@
 const header = document.querySelector(".header");
 const logo = header.querySelector(".header__logo");
-const burgerButton = header.querySelector('.header__burger-btn');
+const burgerOpenButton = header.querySelector('.header__burger-btn');
 const burgerMenu = header.querySelector('.burger-menu');
 const burgerCloseBtn = header.querySelector('.burger-menu__close-button');
+const burgerNavLinks = burgerMenu.querySelectorAll('.burger-menu__nav-link'); 
+const enrollOnCoursesBurgerButton = burgerMenu.querySelector('.burger-menu__button');
 
-burgerButton.addEventListener('click', function() {
-	burgerMenu.classList.add('burger-menu_active');
-});
+function openBurgerMenu() {
+  burgerMenu.classList.add('burger-menu_active');
+}
 
-burgerCloseBtn.addEventListener('click', function() {
-	burgerMenu.classList.remove('burger-menu_active');
-});
+function closeBurgerMenu() {
+  burgerMenu.classList.remove('burger-menu_active');
+}
+
+burgerOpenButton.addEventListener('click', openBurgerMenu);
+burgerCloseBtn.addEventListener('click', closeBurgerMenu);
 
 document.addEventListener("scroll", function () {
   if (window.pageYOffset >= 1) {
@@ -18,4 +23,14 @@ document.addEventListener("scroll", function () {
   } else {
     header.classList.remove("header_second-logo");
   }
+});
+
+burgerNavLinks.forEach(function (link) {
+  link.addEventListener('click', function () {
+    closeBurgerMenu();
+  });
+});
+
+enrollOnCoursesBurgerButton.addEventListener('click', function () {
+  closeBurgerMenu();
 });
