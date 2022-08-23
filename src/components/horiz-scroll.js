20ab3c27c51b0b98fc8326069f663b873bc834c1
Platon Brazhnikov <90373473+BreadLinx@users.noreply.github.com>
2022-08-23 18:48:36 +0300

    section was done

--- src/components/horiz-scroll.js
+++ src/components/horiz-scroll.js
@@ -4,23 +4,30 @@ import { ScrollTrigger } from 'gsap/all';
 gsap.registerPlugin(ScrollTrigger);
 
 const cards = gsap.utils.toArray(".horiz-scroll__card");
+const principles = document.querySelector('.principles');
 
-let matchMedia = gsap.matchMedia();
-
-matchMedia.add("(min-width: 700px)", () => {
-  scrolling()
-})
+const matchMedia = gsap.matchMedia();
+matchMedia.add("(min-width: 1201px)", () => {
+  scrolling();
+});
 
 function scrolling() {
   gsap.to(cards, {
     xPercent: -100 * (cards.length - 1),
     ease: "none",
     scrollTrigger: {
-      trigger: ".horiz-scroll",
-      pin: true,
+      trigger: ".principles",
+      start: "bottom bottom",
+      pin: '.principles',
+      markers: true,
       scrub: 1,
-      snap: 1 / (cards.length - 1),
-      end: () => "+=" + document.querySelector(".horiz-scroll").offsetWidth
+      end: () => `+=${document.querySelector(".horiz-scroll").offsetWidth}`,
     }
-  })
+  });
+  ScrollTrigger.create({
+    trigger: ".principles",
+    start: "bottom bottom",
+    end: () => "+=" + document.querySelector(".horiz-scroll").offsetWidth,
+    onToggle: () => principles.classList.toggle('principles_without-border')
+  });
 }
