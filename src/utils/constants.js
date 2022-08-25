079a545f28a0fdd3dd039f3e080870cc6aafac0b
Platon Brazhnikov <90373473+BreadLinx@users.noreply.github.com>
2022-08-25 22:36:50 +0300

    project has been almost done

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,9 @@
 const popupSelectorCourse = '.popup';
 const popupSelectorPartner = '.popup__partner';
+const burgerMenuOpenButton = document.querySelector('.header__burger-btn');
+const principlesSection = document.querySelector('.principles');
+const burgerMenuLinksArray = document.querySelectorAll('.burger-menu a');
+const popupCourseButton = document.querySelector('.card__button-detail');
+const popupPartnerButtons = document.querySelectorAll('.partners__border');
 
-
-export { popupSelectorCourse, popupSelectorPartner }
+export {popupSelectorCourse, popupSelectorPartner, burgerMenuOpenButton, principlesSection, burgerMenuLinksArray, popupCourseButton, popupPartnerButtons}
